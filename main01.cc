33dc363e53e6775da32859bab7da5b07881c2ff6
imooney <fs3383@wayne.edu>
2017-03-06 00:27:18 -0500

    working split between uncut, cut, and tracking efficiency added histograms

--- main01.cc
+++ main01.cc
@@ -24,7 +24,7 @@ using namespace Pythia8;
 using namespace std;
 
 int main() {
-  // Generator. Process selection. LHC initialization. Histogram.
+  // Generator. Process selection. STAR initialization. Histograms.
   Pythia pythia;
   pythia.readString("Beams:eCM = 200.");
   pythia.readString("HardQCD:all = on");
@@ -75,13 +75,12 @@ int main() {
     TH1D* effic_leadPt = new TH1D("e_leadPt", "pt", 100, 0, 50);
     TH1D* effic_consPt = new TH1D("e_consPt", "constituent pt", 100, 0, 5);
     
-  TH1D* orig = new TH1D("orig", "no efficiency correction", 100, 0, 5);
+  //TH1D* orig = new TH1D("orig", "no efficiency correction", 100, 0, 5);
       
-  //an event worth of pseudojets
-  vector<PseudoJet> uncut_part;
-  vector<PseudoJet> cut_part;
+    //an event worth of pseudojets
+    vector<PseudoJet> uncut_part;
+    vector<PseudoJet> cut_part;
     vector<PseudoJet> effic_part;
-    
   
   //Hist mult("charged multiplicity", 100, -0.5, 799.5);
   
@@ -101,92 +100,150 @@ int main() {
       // Find all final particles and fill histograms
       for (int i = 0; i < pythia.event.size(); ++i) {
         //only select final state particles within particle-level eta cut
-        if (pythia.event[i].isFinal() && abs(pythia.event[i].eta()) < 1) {
-          //++nCharged;
-          //if it's the 5th charged particle, exclude it (so we scale charged particles in histogram by 0.8)
-          if (counter == 4 && pythia.event[i].isCharged()) {
-            //reset the counter for later
-            counter = 0;
-            //fill the original jet, skip filling the tracking efficiency applied jet
-            orig_part.push_back(PseudoJet(pythia.event[i].px(),pythia.event[i].py(),pythia.event[i].pz(),pythia.event[i].e()));
-            continue;
-          }
-          else {
-              //fill both
-              orig_part.push_back(PseudoJet(pythia.event[i].px(),pythia.event[i].py(),pythia.event[i].pz(),pythia.event[i].e()));
-              part.push_back(PseudoJet(pythia.event[i].px(),pythia.event[i].py(),pythia.event[i].pz(),pythia.event[i].e()));
-          }
-          //every charged particle increments the counter
-          if (pythia.event[i].isCharged()) {
-              counter ++;
-          }
+        if (pythia.event[i].isFinal()) {
+            uncut_part.push_back(PseudoJet(pythia.event[i].px(),pythia.event[i].py(),pythia.event[i].pz(),pythia.event[i].e()));
+            if (abs(pythia.event[i].eta()) < 1) {
+                //++nCharged;
+                //if it's the 5th charged particle, exclude it (so we scale charged particles in histogram by 0.8)
+                if (counter == 4 && pythia.event[i].isCharged()) {
+                    //reset the counter for later
+                    counter = 0;
+                    //fill the original jet, skip filling the tracking efficiency applied jet
+                    cut_part.push_back(PseudoJet(pythia.event[i].px(),pythia.event[i].py(),pythia.event[i].pz(),pythia.event[i].e()));
+                    continue;
+                }
+                else {
+                    //fill both
+                    cut_part.push_back(PseudoJet(pythia.event[i].px(),pythia.event[i].py(),pythia.event[i].pz(),pythia.event[i].e()));
+                    effic_part.push_back(PseudoJet(pythia.event[i].px(),pythia.event[i].py(),pythia.event[i].pz(),pythia.event[i].e()));
+                }
+                //every charged particle increments the counter
+                if (pythia.event[i].isCharged()) {
+                    counter ++;
+                }
+            }
         }
       }
     
     // run the clustering, extract the jets
-    ClusterSequence cs(part, jet_def);
-    ClusterSequence c_s(orig_part, jet_def);
-    vector<PseudoJet> jets = sorted_by_pt(cs.inclusive_jets());
-    vector<PseudoJet> orig_jets = sorted_by_pt(c_s.inclusive_jets());
+    ClusterSequence uncut(uncut_part, jet_def);
+    ClusterSequence cut(cut_part, jet_def);
+    ClusterSequence effic(effic_part, jet_def);
+    vector<PseudoJet> uncut_jets = sorted_by_pt(uncut.inclusive_jets());
+    vector<PseudoJet> cut_jets = sorted_by_pt(cut.inclusive_jets());
+    vector<PseudoJet> effic_jets = sorted_by_pt(effic.inclusive_jets());
 
     //cout << "Clustering with " << jet_def.description() << '\n';
 
-    //pseudojet axis cut
-    if (abs(jets[0].eta()) < (1 - R)) {
-      //leading jet pt spectrum
-      pt = jets[0].pt();
-      leading_pt->Fill(pt);
-      tout->Fill();
+    //leading jet pt spectra (a) without cuts, (b) with cuts, and (c) with cuts + efficiency
+    //(a):
+      uncut_leadPt->Fill(uncut_jets[0].pt());
+    //(b):
+    if (abs(cut_jets[0].eta()) < (1 - R)) {
+        //leading jet pt spectrum
+        pt = cut_jets[0].pt();
+        cut_leadPt->Fill(cut_jets[0].pt());
+        tout->Fill();
     }
-    //pseudojets with charged particle tracking efficiency selection
-    for (int i = 0; i < jets.size(); ++ i) {
+    //(c):
+    if (abs(effic_jets[0].eta()) < (1 - R)) {
+        effic_leadPt->Fill(effic_jets[0].pt());
+    }
+      
+      //pseudojets without cuts
+      for (int i = 0; i < uncut_jets.size(); ++ i) {
+        uncut_px->Fill(uncut_jets[i].px());
+        uncut_py->Fill(uncut_jets[i].py());
+        uncut_pz->Fill(uncut_jets[i].pz());
+        uncut_E->Fill(uncut_jets[i].e());
+        uncut_phi->Fill(uncut_jets[i].phi());
+        uncut_eta->Fill(uncut_jets[i].eta());
+        uncut_consPt->Fill(uncut_jets[i].pt());
+      }
+      
+    //pseudojets with cuts
+    for (int i = 0; i < cut_jets.size(); ++ i) {
       //pseudojet axis cut
-      if (abs(jets[i].eta()) < (1 - R)) {
-	px = jets[i].px();                                                                                                                  
-	py = jets[i].py();                                                                                                                  
-	pz = jets[i].pz();                                                                                                                  
-	E = jets[i].e();                                                                                                                    
-	phi = jets[i].phi();                                                                                                                
-	eta = jets[i].eta();
-	con_pt = jets[i].pt();
-	tout->Fill();                                                                                                                           
-	pseudojet_px->Fill(jets[i].px());                                                                                                   
-	pseudojet_py->Fill(jets[i].py());      
-	pseudojet_pz->Fill(jets[i].pz());                                                                                                   
-	pseudojet_E->Fill(jets[i].e());                                                                                                     
-	pseudojet_phi->Fill(jets[i].phi());
-	pseudojet_eta->Fill(jets[i].eta());
-	constituent_pt->Fill(jets[i].pt());
+      if (abs(cut_jets[i].eta()) < (1 - R)) {
+          px = cut_jets[i].px();
+          py = cut_jets[i].py();
+          pz = cut_jets[i].pz();
+          E = cut_jets[i].e();
+          phi = cut_jets[i].phi();
+          eta = cut_jets[i].eta();
+          con_pt = cut_jets[i].pt();
+          tout->Fill();
+          
+          cut_px->Fill(cut_jets[i].px());
+          cut_py->Fill(cut_jets[i].py());
+          cut_pz->Fill(cut_jets[i].pz());
+          cut_E->Fill(cut_jets[i].e());
+          cut_phi->Fill(cut_jets[i].phi());
+          cut_eta->Fill(cut_jets[i].eta());
+          cut_consPt->Fill(cut_jets[i].pt());
       }
     }
+    
+    //pseudojets with cuts + charged particle tracking efficiency selection
+    for (int i = 0; i < effic_jets.size(); ++ i) {
+        if (abs(effic_jets[i].eta()) < (1 - R)) {
+            effic_px->Fill(effic_jets[i].px());
+            effic_py->Fill(effic_jets[i].py());
+            effic_pz->Fill(effic_jets[i].pz());
+            effic_E->Fill(effic_jets[i].e());
+            effic_phi->Fill(effic_jets[i].phi());
+            effic_eta->Fill(effic_jets[i].eta());
+            effic_consPt->Fill(effic_jets[i].pt());
+        }
+    }
+      
+      /*
     //pseudojets without charged particle tracking efficiency selection
     for (int i = 0; i < orig_jets.size(); ++ i) {
       //pseudojet axis cut
       if (abs(orig_jets[i].eta()) < (1 - R)) {
-	orig->Fill(orig_jets[i].pt());
+          orig->Fill(orig_jets[i].pt());
       }
     }
-
+       */
     //mult.fill( nCharged );
     
   // End of event loop. Statistics. Histogram. Done.
   }
-  pythia.stat();
-  //cout << mult << '\n';
+    pythia.stat();
+    //cout << mult << '\n';
   
-  cout << "Writing to " << fout->GetName() << '\n';
-  //save histograms to output root file
-  //tout->Print();
-  tout->Write();
-  pseudojet_px->Write();
-  pseudojet_py->Write();
-  pseudojet_pz->Write();
-  pseudojet_E->Write();
-  pseudojet_phi->Write();
-  pseudojet_eta->Write();
-  leading_pt->Write();
-  orig->Write();
-  constituent_pt->Write();
+    cout << "Writing to " << fout->GetName() << '\n';
+    //save histograms to output root file
+    //tout->Print();
+    tout->Write();
+    uncut_px->Write();
+    uncut_py->Write();
+    uncut_pz->Write();
+    uncut_E->Write();
+    uncut_phi->Write();
+    uncut_eta->Write();
+    uncut_leadPt->Write();
+    uncut_consPt->Write();
+    
+    cut_px->Write();
+    cut_py->Write();
+    cut_pz->Write();
+    cut_E->Write();
+    cut_phi->Write();
+    cut_eta->Write();
+    cut_leadPt->Write();
+    cut_consPt->Write();
+    
+    effic_px->Write();
+    effic_py->Write();
+    effic_pz->Write();
+    effic_E->Write();
+    effic_phi->Write();
+    effic_eta->Write();
+    effic_leadPt->Write();
+    effic_consPt->Write();
+    //orig->Write();
   
   return 0;
 }
